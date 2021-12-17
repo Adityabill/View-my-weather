abcab1dc8c99e10725d2768cc068d75087e25816
Adityabill <adityabikrambill@gmail.com>
2021-12-17 14:59:51 +0530

    Worked on API integration

--- src/components/WeatherResults.js
+++ src/components/WeatherResults.js
@@ -4,14 +4,17 @@ export default class WeatherResult extends React.Component{
     constructor(){
         super();
         this.state={
-            weather_result: null
+            temperature: null,
+            humidity: null,
+            location: null
         }
     }
 
     componentDidMount(){
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.props.city}&appid=4149f38a602563200c95016a01cd334b`).then((response)=>{
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.props.city}&appid=4149f38a602563200c95016a01cd334b&units=metric`).then((response)=>{
             response.json().then((output)=>{
                 console.warn(output)
+                this.setState({temperature: output.main.temp, humidity: output.main.humidity, location: output.name})
             })
         }
         )
@@ -19,7 +22,24 @@ export default class WeatherResult extends React.Component{
     render(){
         return(
             <div>
-                <p>{this.props.city}</p>
+                <p>{this.state.location}</p>                
+                
+                <div>
+                    <p>Temperature: {this.state.temperature}&deg;C</p>
+                    <p>{this.state.humidity}</p>
+                    {/* {
+                        this.state.weather_results?
+                        this.state.weather_results.map((weather_result)=>
+                           <div>
+                               <p>Temperature: {weather_result.main.temp}</p>
+                           </div>
+                        ):
+
+                        <p>City not found</p>
+
+                    }        */}             
+                </div>
+                
             </div>
         )
     }
