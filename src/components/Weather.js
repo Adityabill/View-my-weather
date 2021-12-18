4f9d8367aa037ce40cd968749b75bf83333971d0
Adityabill <adityabikrambill@gmail.com>
2021-12-18 17:56:26 +0530

    Made Weather result function in Weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,8 @@
 import React from "react"
-import {InputGroup, FormControl} from "react-bootstrap"
+import {InputGroup, FormControl, Button} from "react-bootstrap"
 import SearchIcon from './Images/SearchIcon.jpg'
-import WeatherResult from "./WeatherResults";
+import WeatherResult from "./WeatherResults"
+import Axios from "axios"
 
 export default class Weather extends React.Component{
     constructor(){
@@ -10,6 +11,12 @@ export default class Weather extends React.Component{
             city: 'Kolkata'
         }
     }
+    WeatherOutput(){
+        Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=4149f38a602563200c95016a01cd334b&units=metric`).then((response)=>{
+            console.warn(response);
+        });
+        /* alert("Weather") */
+    }
     render(){
         return(
             <div className="container">
@@ -17,6 +24,7 @@ export default class Weather extends React.Component{
                 <InputGroup className="mb-2">
                     <InputGroup.Text><img src={SearchIcon} height="15px" width="15px" alt="Search Icon" /></InputGroup.Text>
                           <FormControl id="inlineFormInputGroup" name="city" onChange={(b)=>{this.setState({city: b.target.value})}} placeholder="Enter name of city. For example: Kolkata, Dibrugarh etc..." />
+                          <Button onClick={()=>{this.WeatherOutput()}}>Search</Button>
                     </InputGroup>
                     <p>The city you searched for: <strong>{this.state.city}</strong></p>
 
--- src/components/WeatherResults.js
+++ src/components/WeatherResults.js
@@ -1,32 +1,41 @@
 import React from "react";
+import Axios from "axios";
 
 export default class WeatherResult extends React.Component{
     constructor(){
         super();
-        this.state={
+        /* this.state={
             temperature: null,
             humidity: null,
             location: null
-        }
+        } */
     }
 
-    componentDidMount(){
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.props.city}&appid=4149f38a602563200c95016a01cd334b&units=metric`).then((response)=>{
-            response.json().then((output)=>{
-                console.warn(output)
-                this.setState({temperature: output.main.temp, humidity: output.main.humidity, location: output.name})
-            })
-        }
-        )
-    }
+    /* WeatherOutput = () => {
+        Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.props.city}&appid=4149f38a602563200c95016a01cd334b&units=metric`).then((response)=>{
+            console.warn(response);
+        });
+    } */
+
+    //WeatherOutput(){
+            /* componentDidMount(){
+                fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.props.city}&appid=4149f38a602563200c95016a01cd334b&units=metric`).then((response)=>{
+                    response.json().then((output)=>{
+                        console.warn(output)
+                        this.setState({temperature: output.main.temp, humidity: output.main.humidity, location: output.name})
+                    })
+                }
+                )
+            } */
+    //}
     render(){
         return(
             <div>
-                <p>{this.state.location}</p>                
+                {/* <p>{this.state.location}</p>    */}             
                 
                 <div>
-                    <p>Temperature: {this.state.temperature}&deg;C</p>
-                    <p>{this.state.humidity}</p>
+                    {/* <p>Temperature: {this.state.temperature}&deg;C</p>
+                    <p>{this.state.humidity}</p> */}
                     {/* {
                         this.state.weather_results?
                         this.state.weather_results.map((weather_result)=>
