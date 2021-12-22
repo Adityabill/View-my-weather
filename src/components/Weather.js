35e9d5ca362a69b92d3342be4d004cda40363018
Adityabill <adityabikrambill@gmail.com>
2021-12-22 18:15:24 +0530

    Added country codes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -12,28 +12,37 @@ export default class Weather extends React.Component{
             temp: null,
             humidity: null,
             feels_like: null,
-            location: null
+            location: null,
+            status_code: null,
+            country_code: null,
         }
     }
     
     //Function to fetch weather data from API 
     WeatherOutput(){
         Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=4149f38a602563200c95016a01cd334b&units=metric`).then((response)=>{
-            console.warn(response.data.cod)
+            console.warn(response.data.sys.country)
             this.setState(
                 {
                     temp: response.data.main.temp, 
                     humidity: response.data.main.humidity,
                     feels_like: response.data.main.feels_like,
                     location: response.data.name,
+                    status_code: response.data.cod,
+                    country_code: response.data.sys.country,
 
                 })
         });
-        /* alert("Weather") */
+
+        if(this.state.status_code=="404"){
+            alert("City you searched is not available or invalid. Please check the spellings.")
+        }
+        
     }
     render(){
         return(
             <div className="container">
+                
 
                 {/* Search Bar */}
                 <InputGroup className="mb-2">
@@ -45,10 +54,12 @@ export default class Weather extends React.Component{
 
                     <div>
                         <WeatherResult //props
-                        search_city={this.state.location}
-                        temperature={this.state.temp}
-                        feels_like_data={this.state.feels_like}
-                        humidity_data={this.state.humidity}
+                            search_city={this.state.location}
+                            temperature={this.state.temp}
+                            feels_like_data={this.state.feels_like}
+                            humidity_data={this.state.humidity}
+                            status_code_data={this.state.status_code}
+                            country_code_data={this.state.country_code}
                         />
                     </div>
             </div>
--- src/components/WeatherResults.js
+++ src/components/WeatherResults.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Axios from "axios";
+//import Axios from "axios";
 
 export default class WeatherResult extends React.Component{
     constructor(){
@@ -8,7 +8,9 @@ export default class WeatherResult extends React.Component{
             temperature_default: null,
             humidity_default: null,
             location_default: null,
-            feels_like_data_default: null
+            feels_like_data_default: null,
+            status_code_d: null,
+            country_code_default: null,
         }
     }
 
@@ -23,6 +25,8 @@ export default class WeatherResult extends React.Component{
                     humidity_default: output.main.humidity, 
                     location_default: output.name,
                     feels_like_data_default: output.main.feels_like,
+                    //status_code_d: output.cod
+                    country_code_default: output.sys.country,
                 })
             })
         }
@@ -36,19 +40,21 @@ export default class WeatherResult extends React.Component{
                 this.props.search_city?(
                 //Data to be rendered if search is made
                 <div>          
-                    {this.props.search_city}<br/>
+                    {this.props.search_city}, {this.props.country_code_data}<br/>
                     Current Temperature: {this.props.temperature}&deg;C &ensp;
                     Feels Like: {this.props.feels_like_data}&deg;C<br/>
                     Humidity: {this.props.humidity_data} %<br/>
+                    {/* Status Code: {this.props.status_code_data} */}
                 </div>
                 ):
                 
                 //Data to be rendered is no search is made(default)
                 <div>
-                    {this.state.location_default}<br/>
+                    {this.state.location_default}, {this.state.country_code_default}<br/>
                     Current Temperature: {this.state.temperature_default}&deg;C &ensp;
                     Feels Like: {this.state.feels_like_data_default}&deg;C<br/>
                     Humidity: {this.state.humidity_default} %<br/>
+                    {/* Status Code: {this.state.status_code_d} */}
                 </div>
 
                 }
